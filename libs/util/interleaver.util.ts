3065291b8f3b9940dfdac71592d3cc5d817c34f7
June <easdkr@daum.net>
2024-06-06 19:14:54 +0900

    [refactor] position pointer 클래스 분리[D

--- libs/util/interleaver.util.ts
+++ libs/util/interleaver.util.ts
@@ -2,39 +2,20 @@ import { identity, pipe } from "fp-ts/function";
 import { flatten, mapWithIndex, filterMap } from "fp-ts/Array";
 import { none, some, type None, type Some } from "fp-ts/Option";
 import { takeFrom } from "../functional";
+import { PositionPointer } from "./position";
 
 export class Interleaver<T> {
   /**
    * 각 배열의 현재 위치를 저장합니다.
    */
-  private positions: number[] = [];
+  private positions!: PositionPointer;
 
-  private constructor(
-    private readonly pattern: number[],
-    private readonly arrays: T[][]
-  ) {
-    this.validateInitialization();
-    this.initializePositions();
+  private constructor(pattern: number[], private readonly arrays: T[][]) {
+    this.positions = PositionPointer.of(
+      arrays.map((a) => a.length),
+      pattern
+    );
   }
-
-  /**
-   * 패턴과 배열의 유효성을 검사합니다.
-   */
-  private validateInitialization = () => {
-    if (this.pattern.length !== this.arrays.length) {
-      throw new Error("패턴과 배열의 길이가 일치하지 않습니다.");
-    }
-
-    if (this.pattern.some((p) => p < 1)) {
-      throw new Error("패턴은 1 이상의 값을 가져야 합니다.");
-    }
-  };
-
-  /**
-   * 각 배열의 현재 위치를 0(첫 번째 요소)으로 초기화합니다.
-   */
-  private initializePositions = () =>
-    (this.positions = this.arrays.map(() => 0));
 
   public static of<T>(pattern: number[], arrays: T[][]): Interleaver<any> {
     return new Interleaver(pattern, arrays);
@@ -62,7 +43,7 @@ export class Interleaver<T> {
       return;
     }
 
-    this.updatePositions();
+    this.positions.update(this.arrays);
 
     // 이번 반복에서 만든 요소를 방출
     yield* nextElements;
@@ -76,7 +57,7 @@ export class Interleaver<T> {
    */
   private getNextElements = (): T[] =>
     pipe(
-      this.pattern,
+      this.arrays,
       mapWithIndex(this.getNextElement),
       filterMap(identity),
       flatten
@@ -86,20 +67,13 @@ export class Interleaver<T> {
    * 현재 위치에 따라 idx번째 배열에서 다음 요소들을 해당 배열의 pattern 크기만큼 가져옵니다.
    */
   private getNextElement = (idx: number): None | Some<T[]> =>
-    this.positions[idx] >= this.arrays[idx].length
+    this.positions.isEnd(idx)
       ? none
       : some(
-          takeFrom(this.positions[idx], this.pattern[idx], this.arrays[idx])
+          takeFrom(
+            this.positions.getPosition(idx),
+            this.positions.getPattern(idx),
+            this.arrays[idx]
+          )
         );
-
-  /**
-   * 다음 위치를 패턴에 따라 업데이트합니다.
-   */
-  private updatePositions(): void {
-    this.positions = this.positions.map((position, idx) =>
-      position + this.pattern[idx] >= this.arrays[idx].length
-        ? this.arrays[idx].length
-        : position + this.pattern[idx]
-    );
-  }
 }
---
+++ libs/util/position.ts
@@ -0,0 +1,47 @@
+export class PositionPointer {
+  #positions: number[] = [];
+
+  private constructor(
+    private readonly lengths: number[],
+    private readonly pattern: number[]
+  ) {
+    this.validatePattern();
+    this.initializePositions();
+  }
+
+  private validatePattern = () => {
+    if (this.pattern.length !== this.lengths.length) {
+      throw new Error("패턴과 배열의 길이가 일치하지 않습니다.");
+    }
+    if (this.pattern.some((p) => p < 1)) {
+      throw new Error("패턴은 1 이상의 값을 가져야 합니다.");
+    }
+  };
+
+  private initializePositions = () =>
+    (this.#positions = this.lengths.map(() => 0));
+
+  public static of(lengths: number[], pattern: number[]): PositionPointer {
+    return new PositionPointer(lengths, pattern);
+  }
+
+  public getPosition(index: number): number {
+    return this.#positions[index];
+  }
+
+  public isEnd(index: number): boolean {
+    return this.#positions[index] >= this.lengths[index];
+  }
+
+  public update<T>(arrays: T[][]): void {
+    this.#positions = this.#positions.map((pos, idx) =>
+      pos + this.pattern[idx] >= arrays[idx].length
+        ? arrays[idx].length
+        : (pos += this.pattern[idx])
+    );
+  }
+
+  public getPattern(index: number): number {
+    return this.pattern[index];
+  }
+}
