f0b0e6804877f9da5c6df112f829be8a28b7b871
June <easdkr@daum.net>
2024-06-06 22:22:44 +0900

    [refactor] 포인터 클래스 주석 추가

--- libs/util/position.ts
+++ libs/util/position.ts
@@ -24,13 +24,25 @@ export class PositionPointer {
   public static of = (lengths: number[], pattern: number[]): PositionPointer =>
     new PositionPointer(lengths, pattern);
 
+  /**
+   * {index} 위치 포인터를 반환합니다.
+   */
   public getPosition = (index: number) => this.#positions[index];
 
+  /**
+   * {index} 위치의 패턴을 반환합니다.
+   */
   public getPattern = (index: number) => this.pattern[index];
 
+  /**
+   * {index} 위치 포인터가 배열의 끝에 도달했는지 여부를 반환합니다.
+   */
   public isEnd = (index: number) =>
     this.#positions[index] >= this.lengths[index];
 
+  /**
+   * pattern에 따라 위치 포인터를 다음 위치로 이동합니다.
+   */
   public next = () =>
     (this.#positions = this.#positions.map((pos, idx) =>
       pos + this.pattern[idx] >= this.lengths[idx]
